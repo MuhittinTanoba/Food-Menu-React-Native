45cd09047eb8112178c7e7b05b4c500cfac62f57
Muhittin Tanoba <muhitan2002@gmail.com>
2023-08-15 09:21:43 +0300

    Navigation for Overview Screen

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Appearance } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+
+
+const Stack = createNativeStackNavigator();
+const colorScheme = Appearance.getColorScheme();
 
 export default function App() {
   return (
     <>
-    <StatusBar style='light'/>
-    <CategoriesScreen />
+      <StatusBar style={colorScheme && "light" ?  "dark" : "light"}/>
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen name="Meals Categories" component={CategoriesScreen}/>
+          <Stack.Screen name="Meal Overview" component={MealsOverviewScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  }
+  container: {},
 });
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,13 @@
 import { View, Pressable, Text, StyleSheet, Platform } from "react-native";
 
-const CategoryGridTile = ({title, color}) => {
+const CategoryGridTile = ({ title, color, onPress }) => {
   return (
-    <View style={[styles.gridItem,  {backgroundColor: color}]}>
-      <Pressable android_ripple={{color: "#ccc"}} style={styles.button}>
+    <View style={[styles.gridItem, { backgroundColor: color }]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={styles.button}
+        onPress={onPress}
+      >
         <View style={styles.innerContainer}>
           <Text style={styles.title}>{title}</Text>
         </View>
@@ -14,28 +18,27 @@ const CategoryGridTile = ({title, color}) => {
 
 export default CategoryGridTile;
 
-
 const styles = StyleSheet.create({
-    gridItem: {
-        flex:1,
-        margin: 16,
-        height: 150,
-        borderRadius: 8,
-        elevation: 4,
-        overflow: Platform.OS === 'android' ? "hidden" : "visible",
-    },
-    button: {
-        flex:1,
-    },
-    innerContainer: {
-        flex:1,
-        padding: 16,
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 8,
-    },
-    title: {
-        fontWeight: "bold",
-        fontSize: 16,
-    }
-})
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
